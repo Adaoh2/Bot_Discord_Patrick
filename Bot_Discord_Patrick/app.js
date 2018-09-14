307a0bccbbd61dc1daa63d5bbf80d22935302f32
Martin Vincent <info@martinvincent.com>
2018-09-14 08:28:15 -0400

    fixed untracked files

---
+++ Bot_Discord_Patrick/app.js
@@ -0,0 +1,99 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Dès que la constante 'client' est initialisée (avec 'new Discord.Client()')
+// on peut y accéder, on peut donc l'utiliser pour lire config.json et donc la valeur 
+// de config.token
+const config = require("./config.json");
+client.login(config.token);
+
+
+const commandfile1 = require('./play.js') //if commandfile1 is in the same folder as this file
+//var command = ""
+
+
+// Ffonction principale du programme. Elle écoute en permanence ce qui se passe dans la
+// chatroom Discord, et répond aux messages qui correspondent aux commandes ci-dessous.
+client.on("message", message => {
+
+    // Your commands here
+    // Result in: If the user's message contains "ping" then
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    // Le préfixe permet au bot de savoir que la commande lui est addressée
+    let prefix = '.';
+
+
+    ///////////////////////////////////////////////////////////////////////////
+    // les commandes en tant que telles sont ci-dessous
+
+    // PING ///////
+    if (message.content.startsWith(prefix + "ping")) {
+        message.channel.send("Pong!");
+    };
+
+
+    // MP ///////
+    if (message.content.startsWith(prefix + "mp")) {
+        message.author.send("Here is a private message !")
+    };
+
+
+    // HELP ///////
+    if (message.content.startsWith(prefix + "help")) {
+        message.author.send("")
+    };
+
+
+	
+    // DATE ///////
+    if (message.content.startsWith(prefix + "date")) {
+        let dates = ["Nous sommes le 83 Septembre 2942.", 
+		     "Nous sommes le 12 Novembre 1983.", 
+		     "Nous sommes le 32 Décembre sur Terre.", 
+		     "Nous sommes le 57 Janvier 1773.", 
+		     "Nous sommes le 30 Février 2465,7."];
+     
+	let date = Math.floor(Math.random() * dates.length);
+        message.channel.send(dates[date]);
+
+    };
+
+    // MERCI ///////
+    if(message.content == "merci") {
+        let mercis = ["De rien", "C'est normal", "Merci qui ?? ;)", "Alors, on dit merci, mais pas s'il vous plaît ? Rhololo les jeunes aujourd'hui :O"];
+        let merci = Math.floor(Math.random() * mercis.length);
+        message.channel.send(mercis[merci]);
+
+    };
+
+
+
+    // SAY ///////
+    if (message.content.startsWith(prefix + "say")) {
+        // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+        // To get the "message" itself we join the `args` back into a string with spaces: 
+        const sayMessage = args.join(" ");
+        // Then we delete the command message (sneaky, right?). 
+	// The catch just ignores the error with a cute smiley thing.
+        message.delete().catch(O_o => {});
+        // And we get the bot to say the thing: 
+        message.channel.send(sayMessage);
+    };
+   
+
+
+    // PLAY ///////
+	if (message.content.startsWith(prefix + "play")) {
+		let command = ""
+		for (command in commandfile1){
+			let cmdmsg = ""
+			cmdmsg = message.content.replace(prefix, '')
+			if (cmdmsg.startsWith(commandfile1[command])){
+				commandfile1[command].variableToCallToRunFunction(arguements) //usually just .run()
+			} 
+		}
+	};
+
+});
